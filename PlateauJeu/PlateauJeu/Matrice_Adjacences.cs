f6ecd3dce14d6e3928eb6524a76b38c075cc4102
ASUS\CEDRIC <ced8014@gmail.com>
2017-03-02 14:43:16 +0100

    [AJOUT]Class Matrices Adjacences
    Constructeur + Signatures méthodes

---
+++ PlateauJeu/PlateauJeu/Matrice_Adjacences.cs
@@ -0,0 +1,15 @@
+﻿using PlateauJeu.Class_Cartes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlateauJeu.Matrice_Adjacences
+{
+    class Matrice_Adjacences
+    {
+        int[] matrice = 
+
+    }
+}
